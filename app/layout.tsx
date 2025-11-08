190caa6e1b39c98c8d24af558d8b5a18597c177c
Mitchell Moore <mmoorejr24@gmail.com>
2025-11-08 12:36:48 -0800

    Step 4: first convex mutation syncUser.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ConvexClientProviderWithClerk } from "@/components/providers/ConvexClientProviderWithClerk";
 import Header from "@/components/shared/Header";
+import SyncUserWithConvex from "@/components/SyncUserWithConvex";
 
 export const metadata: Metadata = {
   title: "YT Ticket App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="min-h-screen flex flex-col w-full">
         <ConvexClientProviderWithClerk>
           <Header />
+          <SyncUserWithConvex />
           {children}
         </ConvexClientProviderWithClerk>
       </body>
